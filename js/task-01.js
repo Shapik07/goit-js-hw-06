03d4a16cacfdd957080aa4f69d63982092d62d3b
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-21 14:26:51 +0300

    Commit

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const countNumberCategories = function () {
+    const item = document.querySelectorAll(".item");
+    console.log(`Number of categories: ${item.length}`)
+};
+
+countNumberCategories();
+
+const showsCategoriesAndItems = function () {
+    const items = document.querySelectorAll('li.item')
+    console.log(items)
+    
+};
+
+showsCategoriesAndItems()
+
+
