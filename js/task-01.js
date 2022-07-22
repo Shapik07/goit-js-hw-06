03c40994496bb47b7d7849627f0376f47cb5a089
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-22 09:08:54 +0300

    Update

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,18 @@
 const countNumberCategories = function () {
-    const item = document.querySelectorAll(".item");
-    console.log(`Number of categories: ${item.length}`)
+  const item = document.querySelectorAll(".item");
+  console.log(`Number of categories: ${item.length}`);
 };
 
 countNumberCategories();
 
 const showsCategoriesAndItems = function () {
-    const item = document.querySelector(".item");
-    const headers = document.querySelectorAll(".item > h2");
-    // console.log(`Category: ${headers.textContent}`); выводит в консоль то что нужно
-    console.log(headers);
-    for (const header of headers) {
-        console.log(`Category: ${header.textContent}`);
-    }
-
+  const headlines = document.querySelectorAll(".item > h2");
+  for (const header of headlines) {
+    console.log(`Category: ${header.textContent}`);
+    const item = document.querySelector(".item > ul");
+    console.log(`Elements: ${item.children.length}`);
+  }
 };
 
-showsCategoriesAndItems()
+showsCategoriesAndItems();
 
