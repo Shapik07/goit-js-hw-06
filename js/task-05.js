855a101f622217e6adf6c24716f7cf51fe055298
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-23 11:38:49 +0300

    Update

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener(
+    "input", (event) => {
+        event.currentTarget.value === "" ? output.textContent = "Anonymous" : output.textContent = event.currentTarget.value;
+    }
+);
+
+    //     if (event.currentTarget.value === '') {
+    //         output.textContent = "Anonymous";
+    //     } else {
+    //         output.textContent = event.currentTarget.value;
+    //     }
