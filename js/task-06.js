e769fd85703f5bd9210632974afb3322711f16b6
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-23 13:55:00 +0300

    Update

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,19 @@
 const inputRef = document.querySelector("#validation-input");
-console.log(inputRef)
+console.log(inputRef);
 const lengthLine = inputRef.getAttribute("data-length");
 console.log(lengthLine);
 
+inputRef.addEventListener("blur", () => {
+  inputRef.value.length < lengthLine
+    ? inputRef.classList.add("invalid")
+    : inputRef.classList.remove("invalid"),
+    inputRef.classList.add("valid");
 
+  // if (inputRef.value.length < lengthLine) {
+  //     inputRef.classList.add("invalid");
+  // } else {
+  //     inputRef.classList.remove("invalid");
+  //     inputRef.classList.add("valid");
+  // }
+});
 
-inputRef.addEventListener('blur', () => {
-    if (inputRef.value < lengthLine) {
-        
-    }
-} )
