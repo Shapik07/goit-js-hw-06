369c10913f9d665a34d3b4ae3284f8443cc56426
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-24 12:45:39 +0300

    Update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+      const message = "Друг, все поля должны быть заполнены:)";
+      alert(message);
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`)
+    }
+
+formRef.addEventListener('submit', handleSubmit)
