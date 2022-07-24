592fd4491e77d774d884e90aab5313f8f2d9f21d
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-24 14:17:32 +0300

    Update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const pageRef = document.querySelector("html");
+const bodyRef = document.querySelector('.body')
+const buttonRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+function changeBodyColor() {
+  pageRef.lastChild.style.backgroundColor = getRandomHexColor();
+}
+
+buttonRef.addEventListener('click', changeBodyColor)
