484e060f9a99a6a1e755cc283645a28401708bdc
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-27 12:18:37 +0300

    Update

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ const lengthLine = inputRef.getAttribute("data-length");
 console.log(lengthLine);
 
 inputRef.addEventListener("blur", () => {
-  inputRef.value.length < lengthLine
+  inputRef.value.length < lengthLine || inputRef.value.length > lengthLine
     ? inputRef.classList.add("invalid")
     : inputRef.classList.remove("invalid"),
     inputRef.classList.add("valid");
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createButtonRef = document.querySelector("[data-create]");
+const destroyButtonRef = document.querySelector("[data-destroy]");
+const createNumberOfElements = document.querySelector("input");
+const divBoxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+divBoxes.createElement("div");
+}
