5a1f443a7bbb1ac98771dbeb50b100b3f25f0440
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-27 12:20:05 +0300

    Update

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputRef = document.querySelector("#font-size-control");
 const textRef = document.querySelector("#text");
 
+textRef.style.fontSize = inputRef.value + "px";
+
 const textSize = function() {
 textRef.style.fontSize = inputRef.value + 'px'
 }
